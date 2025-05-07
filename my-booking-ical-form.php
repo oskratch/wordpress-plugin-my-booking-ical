bbcc32d8f3433d18162f6ccabf0072364d795356
Oscar Periche <oskratch@gmail.com>
2025-05-07 11:47:47 +0200

    chore(db): set table engine to InnoDB for better WordPress compatibility and support for transactions

--- my-booking-ical-form.php
+++ my-booking-ical-form.php
@@ -52,7 +52,7 @@ function mbif_plugin_enable() {
     $sql_1 .= "`price` decimal(5,2) NOT NULL,";
     $sql_1 .= "`max_capacity` int(2) NOT NULL,";
     $sql_1 .= "`parking_option` tinyint(1) NOT NULL DEFAULT '0'";
-    $sql_1 .= ") ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+    $sql_1 .= ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 
     dbDelta($sql_1);
     
@@ -61,19 +61,19 @@ function mbif_plugin_enable() {
     $sql_2 = "CREATE TABLE `" . $table_name_2 . "` (";
     $sql_2 .= "`id` int(10) NOT NULL AUTO_INCREMENT PRIMARY KEY,";
     $sql_2 .= "`form_id` int(5) NOT NULL,";
-    $sql_2 .= "`first_name` varchar(80) COLLATE utf8_spanish_ci NOT NULL,";
-    $sql_2 .= "`last_name` varchar(120) COLLATE utf8_spanish_ci NOT NULL,";
-    $sql_2 .= "`email` varchar(80) COLLATE utf8_spanish_ci NOT NULL,";
-    $sql_2 .= "`phone` varchar(30) COLLATE utf8_spanish_ci,";
+    $sql_2 .= "`first_name` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,";
+    $sql_2 .= "`last_name` varchar(120) COLLATE utf8mb4_unicode_ci NOT NULL,";
+    $sql_2 .= "`email` varchar(80) COLLATE utf8mb4_unicode_ci NOT NULL,";
+    $sql_2 .= "`phone` varchar(30) COLLATE utf8mb4_unicode_ci,";
     $sql_2 .= "`guest_count` int(2) NOT NULL,";
     $sql_2 .= "`entry_date` date NOT NULL,";
     $sql_2 .= "`departure_date` date NOT NULL,";
     $sql_2 .= "`parking` tinyint(1) NOT NULL DEFAULT '0',";
-    $sql_2 .= "`comments` text,";
-    $sql_2 .= "`summary` text,";
-    $sql_2 .= "`status` enum('pending_review','validated','denied') NOT NULL DEFAULT 'pending_review',";
+    $sql_2 .= "`comments` text COLLATE utf8mb4_unicode_ci,";
+    $sql_2 .= "`summary` text COLLATE utf8mb4_unicode_ci,";
+    $sql_2 .= "`status` enum('pending_review','validated','denied') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'pending_review',";
     $sql_2 .= "`created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP";
-    $sql_2 .= ") ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+    $sql_2 .= ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 
     dbDelta($sql_2);
     
@@ -85,7 +85,7 @@ function mbif_plugin_enable() {
     $sql_3 .= "`from_date` date NOT NULL,";
     $sql_3 .= "`to_date` date NOT NULL,";
     $sql_3 .= "`price` decimal(5,2) NOT NULL";
-    $sql_3 .= ") ENGINE=MyISAM DEFAULT CHARSET=utf8;";
+    $sql_3 .= ") ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;";
 
     dbDelta($sql_3);
 
@@ -94,7 +94,7 @@ function mbif_plugin_enable() {
     foreach(mbif_Options() as $key => $value){
         add_option($key, $value);
     }
-} 
+}
 
 register_activation_hook(__FILE__, 'mbif_plugin_enable');
 
