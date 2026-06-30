02537800d65dca44ba151ad9aa064f5d05aa6651
Òscar Periche <oskratch@gmail.com>
2026-06-30 15:46:11 +0200

    fix: esc_html on requests list title and clean up dead uninstall code

--- my-booking-ical-form/uninstall.php
+++ my-booking-ical-form/uninstall.php
@@ -3,26 +3,9 @@
  * Copyright (c) 2025 Oscar Periche, Metalinked
  * Licensed under GPL v2 or later
  * https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * Uninstall logic is registered via register_uninstall_hook() in my-booking-ical-form.php
+ * and handled by mbif_plugin_uninstall().
  */
 
-/*
-** Database: Deleting Tables
-*/
-
-function db_plugin_tables() {
-    global $wpdb;
-    
-    include_once ABSPATH . 'wp-admin/includes/upgrade.php';
-
-    $table_name_1 = $wpdb->prefix . "my_booking_ical_forms";
-
-    $sql_1 = "DROP TABLE `" . $table_name_1 . "`";
-
-    dbDelta($sql_1);
-    
-    $table_name_2 = $wpdb->prefix . "my_booking_ical_requests";
-
-    $sql_2 = "DROP TABLE `" . $table_name_2 . "`";
-
-    dbDelta($sql_2);
-} 
+defined('WP_UNINSTALL_PLUGIN') or die;
--- my-booking-ical-form/views/admin/my_booking_ical_requests.php
+++ my-booking-ical-form/views/admin/my_booking_ical_requests.php
@@ -1,5 +1,5 @@
 <div class="wrap">
-  <h1><?php echo $item->title;?></h1>
+  <h1><?php echo esc_html($item->title);?></h1>
   <p><?php echo __('List of requests received for this apartment.', 'my_booking_ical_form')?></p>
   <?php $table->display();?>
 </div>
